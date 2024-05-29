9cfb669a586b2abf4dbd48c39e54bfd5aee1b90d
Dhruba Dawndawndhruba@gmail.com <askjavamentor@gmail.com>
2024-05-29 07:59:05 +0530

    inital commit

---
+++ src/test/java/djd/boot/offer/service/OfferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package djd.boot.offer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
